40a90079215fdeb4060bc6dfdaa9c266ffda2387
Andrew Tate <drewctate@gmail.com>
2018-11-27 15:37:19 -0700

    Routing config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EventCalendarComponent } from './event-calendar/event-calendar.component';
+import { EventFeedComponent } from './event-feed/event-feed.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: MainScreenComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'feed',
+        pathMatch: 'full'
+      },
+      {
+        path: 'feed',
+        component: EventFeedComponent,
+      },
+      {
+        path: 'calendar',
+        component: EventCalendarComponent,
+      }
+    ]
+  },
+  { path: '**', redirectTo: '/' }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCalendarComponent,
+    EventFeedComponent,
+    MainScreenComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/event-feed/event-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-feed',
+  templateUrl: './event-feed.component.html',
+  styleUrls: ['./event-feed.component.scss']
+})
+export class EventFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
