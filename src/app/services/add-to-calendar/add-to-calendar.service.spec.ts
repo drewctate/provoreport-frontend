082d3d891708a802a9f8b3b6c6af3ef00306e37d
Andrew Tate <drewctate@gmail.com>
2019-01-16 17:43:09 -0700

    Fixing/removing tests

--- src/app/services/add-to-calendar/add-to-calendar.service.spec.ts
+++ src/app/services/add-to-calendar/add-to-calendar.service.spec.ts
@@ -17,11 +17,14 @@ describe('AddToCalendarService', () => {
       url: 'https://calendar.byu.edu/event/fall-2018-reason-hope',
       time: '10:00 AM to 4:00 PM',
       location: 'Wilkinson Student Center',
-      thumbnail: 'https://calendar.byu.edu/sites/default/files/styles/thumbnail/public/Other_600x600.jpg?itok=d4gtvndZ', \
-      id: 3
+      startDate: '20190114T190000',
+      endDate: '20190114T190000',
+      thumbnail: 'https://calendar.byu.edu/sites/default/files/styles/thumbnail/public/Other_600x600.jpg?itok=d4gtvndZ',
+      id: '',
+      tags: []
     });
 
-    expect(calendarURL).toEqual('https://www.google.com/calendar/render?action=TEMPLATE&text=Fall 2018 Reason for Hope&dates=20180704T190000/20180704T210000&details=https://calendar.byu.edu/event/fall-2018-reason-hope&location=Wilkinson Student Center')
+    expect(calendarURL).toEqual('https://www.google.com/calendar/render?action=TEMPLATE&text=Fall 2018 Reason for Hope&dates=20190114T190000/20190114T190000&details=https://calendar.byu.edu/event/fall-2018-reason-hope&location=Wilkinson Student Center');
   });
 
 
