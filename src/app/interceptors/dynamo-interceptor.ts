a99158abcea7ed00e5aa3d47f1d2cdeb3b8f2d60
Andrew Tate <drewctate@gmail.com>
2019-02-11 15:42:41 -0700

    converting nones to nulls on frontend

--- src/app/interceptors/dynamo-interceptor.ts
+++ src/app/interceptors/dynamo-interceptor.ts
@@ -4,9 +4,20 @@ import dynamoConverters from 'dynamo-converters';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Event } from '../types';
 
 @Injectable()
 export class DynamoInterceptor implements HttpInterceptor {
+
+  private nonesToNulls(event: Event) {
+    for (const attr in event) {
+      if (event[attr] === 'none') {
+        event[attr] = null;
+      }
+    }
+    return event;
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -16,7 +27,9 @@ export class DynamoInterceptor implements HttpInterceptor {
       map(evt => {
         if (evt instanceof HttpResponse) {
           try {
-            evt = evt.clone({ body: evt.body.Items.map(eventItem => dynamoConverters.itemToData(eventItem)) });
+            const parsedFromDynamo: Event[] = evt.body.Items.map(eventItem => dynamoConverters.itemToData(eventItem));
+            const nonesNowNulls: Event[] = parsedFromDynamo.map(event => this.nonesToNulls(event));
+            evt = evt.clone({ body: nonesNowNulls });
           } catch (err) {
             console.log('Assuming not a Dynamo response; not converting.', err);
           }
