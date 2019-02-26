724ace67e240a794bd4a3fd6ba591920880bf7be
Andrew Tate <drewctate@gmail.com>
2019-02-26 13:45:54 -0700

    Cancelling share dialogue if no events

--- src/app/saved-events/share-dialogue/share-dialogue.component.ts
+++ src/app/saved-events/share-dialogue/share-dialogue.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { EventsService } from 'src/app/services';
 
 @Component({
   selector: 'app-share-dialogue',
   templateUrl: './share-dialogue.component.html',
   styleUrls: ['./share-dialogue.component.scss']
 })
-export class ShareDialogueComponent {
+export class ShareDialogueComponent implements DoCheck {
 
-  constructor(public dialogRef: MatDialogRef<ShareDialogueComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<ShareDialogueComponent>,
+    private eventsService: EventsService
+  ) { }
+
+  ngDoCheck() {
+    if (this.eventsService.savedEvents.length === 0) {
+      this.cancel();
+    }
+  }
 
   public cancel() {
     this.dialogRef.close();
