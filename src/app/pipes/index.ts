2dbcd1164b67bcaebbb27a387812083fa4dc20d5
Andrew Tate <drewctate@gmail.com>
2019-02-26 21:29:33 -0700

    Generalizing sort pipe

--- src/app/pipes/index.ts
+++ src/app/pipes/index.ts
@@ -1,4 +1,4 @@
 export * from './custom-title-case/custom-title-case.pipe';
 export * from './decode-html-string/decode-html-string.pipe';
 export * from './event-tags-filter/event-tags-filter.pipe';
-export * from './sort-events/sort-events.pipe';
+export * from './sort/sort';
---
+++ src/app/pipes/sort/sort.ts
@@ -0,0 +1,41 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
+
+const getSortFunc = (attribute: string, type: 'date' | 'val') => {
+  if (type === 'date') {
+    return (a, b) => {
+      if (moment(a[attribute]).isBefore(moment(b[attribute]))) {
+        return -1;
+      } else if (moment(b[attribute]).isBefore(moment(a[attribute]))) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  } else {
+    return (a, b) => {
+      if (a[attribute] < (b[attribute])) {
+        return -1;
+      } else if (b[attribute] > a[attribute]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  }
+};
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(array: any[], attribute: string, type: 'date' | 'val' = 'val'): any {
+    if (attribute) {
+      return array.sort(getSortFunc(attribute, type));
+    } else {
+      return array.sort();
+    }
+  }
+
+}
