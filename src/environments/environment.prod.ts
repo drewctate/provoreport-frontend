c3825c4936641c36e0543b854f909eb51bbfafa6
Andrew Tate <drewctate@gmail.com>
2019-03-03 22:12:55 -0700

    Ignoring environments

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-import { fuseOptions } from './fuse-options';
-
-export const environment = {
-  production: true,
-  ROOT_URL: 'https://r1e2zr6ezi.execute-api.us-west-2.amazonaws.com/beta',
-  fuseOptions: fuseOptions
-};
