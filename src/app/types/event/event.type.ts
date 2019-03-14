cc0b5ba19426fd50fddee5b8903aea04160a13a1
Andrew Tate <drewctate@gmail.com>
2019-03-13 21:42:23 -0600

    TimeStr

--- src/app/types/event/event.type.ts
+++ src/app/types/event/event.type.ts
@@ -7,6 +7,7 @@ export interface Event {
     ticketsUrl: string;
     startDateTime: string;
     endDateTime: string;
+    timeStr: string;
     thumbnail: string;
     past: string;
     tags: Array<string>;
